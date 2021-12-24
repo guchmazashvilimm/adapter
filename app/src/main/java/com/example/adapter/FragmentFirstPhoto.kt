bb122e9226f4cffb6030dac81bc05d241f3982ea
mariam guchmazashvili <mariami.guchmazashvili.1@btu.edu.ge>
2021-12-24 23:23:09 +0400

    Initial commit

---
+++ app/src/main/java/com/example/adapter/FragmentFirstPhoto.kt
@@ -0,0 +1,8 @@
+package com.example.n6.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.adapter.R
+
+
+class FragmentFirstphoto : Fragment(R.layout.fragment_firstphoto) {
+}
---
+++ app/src/main/java/com/example/adapter/FragmentSecondPhoto.kt
@@ -0,0 +1,7 @@
+package com.example.n6.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.adapter.R
+
+class FragmentSecondphoto : Fragment(R.layout.fragment_secondphoto) {
+}
